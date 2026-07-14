2dd6bae4f4765cb084fc9362fefbee5a9481cab5
yogendra5046 <161818175+yogendra5046@users.noreply.github.com>
2026-07-14 20:46:53 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2= Singleton.getInstance();
-        obj1.showmessage();
-        System.out.println(obj1 == obj2);
-    }
-}
