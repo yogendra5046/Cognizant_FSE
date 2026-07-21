96364c6de70ba15c0395ec4a1f3eed43aadc34bd
yogendra5046 <161818175+yogendra5046@users.noreply.github.com>
2026-07-21 17:49:26 +0530

    Add files via upload

---
+++ spring-rest-handson/spring-rest-handson/src/test/java/com/cognizant/spring_rest_handson/SpringRestHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_rest_handson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
