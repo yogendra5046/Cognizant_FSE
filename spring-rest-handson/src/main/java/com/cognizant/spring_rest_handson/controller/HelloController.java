efca3d484493fc3f6d1bca009e0f7b4e7467205a
yogendra5046 <161818175+yogendra5046@users.noreply.github.com>
2026-07-23 16:59:08 +0530

    Add files via upload

---
+++ spring-rest-handson/src/main/java/com/cognizant/spring_rest_handson/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_rest_handson.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!";
+    }
+}
